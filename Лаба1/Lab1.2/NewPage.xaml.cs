502e6c3f31c564aadd515efc1101274b850aec4c
dmitry-PP <144995392+dmitry-PP@users.noreply.github.com>
2024-03-17 14:27:47 +0300

    Add files via upload

---
+++ Лаба1/Lab1.2/NewPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для NewPage.xaml
+    /// </summary>
+    public partial class NewPage : Page
+    {
+
+        private LibraryDBEntities library = new LibraryDBEntities();
+        public NewPage()
+        {
+            InitializeComponent();
+            Reload();
+        }
+
+        private void Reload()
+        {
+            Lib.ItemsSource = library.Books.ToList();
+            tb.Text = library.Books.ToString();
+        }
+    }
+}
+
---
+++ Лаба1/Lab1.2/NewPage2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для NewPage2.xaml
+    /// </summary>
+    public partial class NewPage2 : Page
+    {
+
+        private LibraryDBEntities library = new LibraryDBEntities();
+        public NewPage2()
+        {
+            InitializeComponent();
+            Reload();
+        }
+
+        private void Reload()
+        {
+            Lib.ItemsSource = library.Authors.ToList();
+            tb.Text = library.Authors.ToString();
+        }
+    }
+}
