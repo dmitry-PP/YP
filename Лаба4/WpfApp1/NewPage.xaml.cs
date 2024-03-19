6abab2b276307aa456e0962196f758f53d92810a
dmitry-PP <144995392+dmitry-PP@users.noreply.github.com>
2024-03-19 21:45:06 +0300

    Add files via upload

---
+++ Лаба4/WpfApp1/NewPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.DataSetLibTableAdapters;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для NewPage.xaml
+    /// </summary>
+    public partial class NewPage : Page
+    {
+        BooksTableAdapter books = new BooksTableAdapter();
+        public NewPage()
+        {
+            InitializeComponent();
+            Lib.ItemsSource = books.GetData();
+            
+
+            filter.DisplayMemberPath = "Name";
+            filter.SelectedValuePath = "ID";
+
+            filter.ItemsSource = GetRelated();
+        }
+
+        private List<Item> GetRelated()
+        {
+            var list = new List<Item>();
+
+            foreach (DataSetLib.AuthorsRow auth in (new AuthorsTableAdapter()).GetData())
+            {
+                string name = "";
+
+                name += auth.SureName + " " + auth.FirstName.Substring(0, 1) + ". " +
+                    ((auth.IsLastNameNull()) ? "" : (auth.LastName + ".")) + "  - " +
+                    auth.Birthday.ToString("yyyy") + $" ({auth.Nationality})";
+
+                list.Add(new Item(auth.ID,name));
+
+            }
+
+            return list;
+            
+
+        }
+
+        private void srch_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Lib.ItemsSource = books.SearchByTitle(search.Text);
+        }
+
+        private void clr_Click(object sender, RoutedEventArgs e)
+        {
+            search.Text = "";
+            filter.SelectedItem = null;
+
+            Lib.ItemsSource = books.GetData();
+        }
+
+        private void filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(filter.SelectedItem != null)
+            {
+                Lib.ItemsSource = books.FilterByAuthor((int)filter.SelectedValue);
+            }
+        }
+    }
+}
+
+class Item
+{
+    public int ID { get; }
+    public string Name { get; }
+
+    public Item(int id, string name)
+    {
+        this.ID = id;
+        this.Name = name;
+    }
+}
---
+++ Лаба4/WpfApp1/NewPage2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.DataSetLibTableAdapters;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для NewPage2.xaml
+    /// </summary>
+    public partial class NewPage2 : Page
+    {
+        AuthorsTableAdapter authors = new AuthorsTableAdapter();
+        public NewPage2()
+        {
+            InitializeComponent();
+            Lib.ItemsSource = authors.GetData();
+
+            foreach (DataSetLib.AuthorsRow auth in Lib.ItemsSource)
+            {
+                if(!filter.Items.Contains(auth.Nationality))
+                {
+                    filter.Items.Add(auth.Nationality);
+                }
+            }
+        }
+
+        private void srch_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Lib.ItemsSource = authors.SearchBySureName(search.Text);
+        }
+
+        private void clr_Click(object sender, RoutedEventArgs e)
+        {
+            search.Text = "";
+            filter.SelectedItem = null;
+
+            Lib.ItemsSource = authors.GetData();
+        }
+
+        private void filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (filter.SelectedItem != null)
+            {
+                Lib.ItemsSource = authors.FilterByNationality((string)filter.SelectedItem);
+            }
+        }
+    }
+}
